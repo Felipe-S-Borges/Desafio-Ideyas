f42bacefb9ced6877afd3cd0b4c4b54af228a026
Felipe <felipelee22@gmail.com>
2022-02-08 21:29:45 -0300

    Script inserção nova linha na tabela

--- Desafio/static/js/scripts.js
+++ Desafio/static/js/scripts.js
@@ -1,10 +1,51 @@
-function insertOnTable(){
-    console.log('work')
+
+
+const tempAmigo = {
+    nome: "Alice",
+    idade:"33",
+    telefone: "27 89562 5522",
+    documento:"RG 56226856"
+}
+
+const addAmigo = (amigo) => {
+    const db_amigo = JSON.parse(localStorage.getItem('db_amigo')) ?? []
+    db_amigo.push(amigo)
+    localStorage.setItem("db_amigo",JSON.stringify(db_amigo))
 }
 
 const abrirModal = () => document.getElementById('modalCadastro').classList.add('active')
 
 const fecharModal = () => document.getElementById('modalCadastro').classList.remove('active')
 
+const salvarAmigo = () => {
+    const amigo = {
+        nome: document.getElementById('nome').value,
+        idade: document.getElementById('idade').value,
+        telefone: document.getElementById('telefone').value,
+        documento: document.getElementById('documento').value
+    }
+    addAmigo(amigo)
+}
+
+const criarLinha = (amigo) => {
+
+    const novaLinha = document.createElement('tr')
+    novaLinha.innerHTML = `
+    <td>${amigo.nome}</td>
+    <td>${amigo.idade}</td>
+    <td>${amigo.telefone}</td>
+    <td>${amigo.documento}</td>
+    `
+    document.querySelector('#listaAmiga>tbody').appendChild(novaLinha)
+}
+
+const atualizarTabela = () => {
+    const db_amigo = JSON.parse(localStorage.getItem('db_amigo')) ?? []
+    db_amigo.forEach(criarLinha)
+}
+
+atualizarTabela()
+
 document.getElementById('adicionarAmigo').addEventListener('click',abrirModal)
 document.getElementById('fecharModal').addEventListener('click',fecharModal)
+document.getElementById('salvar').addEventListener('click',salvarAmigo)
